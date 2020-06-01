eb588d33dfa993078e9fd7906ba061d65a1f83e9
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-06-01 10:33:35 -0500

    phaseOne completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace PlanYourHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your Heist!");
+            Console.WriteLine("Enter Team Member's name");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Enter Team Member's skill level");
+            int newSkillLevel = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Team Member's courage level");
+            double newCourageFactor = Convert.ToDouble(Console.ReadLine());
+
+            TeamMember newTeamMember = new TeamMember()
+            {
+                Name = newName,
+                SkillLevel = newSkillLevel,
+                CourageFactor = newCourageFactor
+
+            };
+            Console.WriteLine(newTeamMember);
+
+
         }
+
     }
 }
